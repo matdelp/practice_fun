b75988b910eb4162b8089598020b922b31b87803
matdelp <mathilde.delpeuch@gmail.com>
2025-08-28 17:39:34 +0200

    feat: works for edge cases

--- merge_intervals/q1.ts
+++ merge_intervals/q1.ts
@@ -1,15 +1,9 @@
 // 👉 You’re given an array of intervals where intervals[i] = [start, end].
 // Merge all overlapping intervals and return the result.
+// // touching intervals count as overlapping
 
 import { log } from "console";
 
-// Input: [[1,3],[2,6],[8,10],[15,18]]
-// Output: [[1,6],[8,10],[15,18]]
-// // because [1,3] and [2,6] overlap → merged into [1,6]
-
-// Input: [[1,4],[4,5]]
-// Output: [[1,5]]
-// // touching intervals count as overlapping
 type Tuple = number[];
 
 const intervalMerger = (list: Tuple[]): Tuple[] => {
@@ -17,9 +11,12 @@ const intervalMerger = (list: Tuple[]): Tuple[] => {
   for (let i = 0; i < list.length; i++) {
     let current: Tuple = list[i];
     let j = i + 1;
-    while (j < list.length && current[1] > list[j][0]) {
-      current = [current[0], list[j][1]];
-      log(current);
+    while (j < list.length && current[1] >= list[j][0]) {
+      if (list[j][1] < current[1]) {
+        current = [current[0], current[1]];
+      } else {
+        current = [current[0], list[j][1]];
+      }
       j++;
     }
     result = result.concat([current]);
@@ -35,6 +32,13 @@ log(
     [2, 6],
     [8, 10],
     [15, 18],
+  ])
+);
+log(
+  "ex2:",
+  intervalMerger([
+    [1, 4],
+    [4, 5],
   ])
 );
 log(
@@ -55,6 +59,3 @@ log(
     [15, 18],
   ])
 );
-// log("ex2:", mergeIntervalifOverlap([1, 2], [2, 6])); //merge
-// log("ex3:", mergeIntervalifOverlap([1, 2], [3, 6])); //don't
-// log("ex4:", mergeIntervalifOverlap([2, 6], [1, 3])); //merge
