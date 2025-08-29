5506308226615430efd7150126dc085596c4ad8f
matdelp <mathilde.delpeuch@gmail.com>
2025-08-29 16:26:41 +0200

    comment

--- leetcode_top150Interview/80_RemoveDuplicatesFromSortedArrayII.ts
+++ leetcode_top150Interview/80_RemoveDuplicatesFromSortedArrayII.ts
@@ -11,21 +11,7 @@ Return k after placing the final result in the first k slots of nums.
 
 Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
 
-Example 1:
-
-Input: nums = [1,1,1,2,2,3]
-Output: 5, nums = [1,1,2,2,3,_]
-Explanation: Your function should return k = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
-It does not matter what you leave beyond the returned k (hence they are underscores).
-
-Example 2:
-
-Input: nums = [0,0,1,1,1,1,2,3,3]
-Output: 7, nums = [0,0,1,1,2,3,3,_,_]
-Explanation: Your function should return k = 7, with the first seven elements of nums being 0, 0, 1, 1, 2, 3 and 3 respectively.
-It does not matter what you leave beyond the returned k (hence they are underscores).
-
- */
+*/
 
 import { log } from "console";
 
