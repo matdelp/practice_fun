73bc97f22f26e1b368e4cef657c7a9e0bdeb1baf
matdelp <mathilde.delpeuch@gmail.com>
2025-09-02 10:06:50 +0200

    simplified breaking records

--- HackerRank_prepInterview/BreakingTheRecord.ts
+++ HackerRank_prepInterview/BreakingTheRecord.ts
@@ -2,24 +2,21 @@ import { log } from "console";
 
 const breakingRecords = (scores: number[]): number[] => {
   if (scores.length < 0) return [0, 0];
-  const records: Map<string, number> = new Map();
-  records.set("min", scores[0]);
-  records.set("breakmin", 0);
-  records.set("max", scores[0]);
-  records.set("breakmax", 0);
-  for (let i = 1; i < scores.length + 1; i++) {
-    const currMin = records.get("min");
-    const currMax = records.get("max");
+  let min = scores[0];
+  let breakmin = 0;
+  let max = scores[0];
+  let breakmax = 0;
 
-    if (scores[i] < currMin!) {
-      records.set("breakmin", records.get("breakmin")! + 1);
-      records.set("min", scores[i]);
-    } else if (scores[i] > currMax!) {
-      records.set("breakmax", records.get("breakmax")! + 1);
-      records.set("max", scores[i]);
+  for (let i = 1; i <= scores.length; i++) {
+    if (scores[i] < min) {
+      breakmin = breakmin + 1;
+      min = scores[i];
+    } else if (scores[i] > max) {
+      breakmax = breakmax + 1;
+      max = scores[i];
     }
   }
-  return [records.get("breakmax")!, records.get("breakmin")!];
+  return [breakmax, breakmin];
 };
 
 log(breakingRecords([12, 24, 10, 24]));
