466d8bd1d1bf2a5c281e9a645c62fce084e7a975
matdelp <mathilde.delpeuch@gmail.com>
2025-09-16 10:24:26 +0200

    little fix

--- HackerRank/CountingValleys.ts
+++ HackerRank/CountingValleys.ts
@@ -4,8 +4,8 @@ const countingValleys = (steps: number, path: string): number => {
   let valleyCount = 0;
   let level = 0;
   Array.from(path).forEach((letter) => {
-    if (letter === "U") level += 1;
-    else level -= 1;
+    if (letter === "U") level++;
+    else level--;
     if (level === 0 && letter != "D") valleyCount += 1;
   });
   return valleyCount;
